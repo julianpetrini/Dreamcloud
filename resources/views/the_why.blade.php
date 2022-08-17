e31f402131e9383e4946b9096026f47a06ecc3a7
julianpetrini <bwmisojos@gmail.com>
2022-08-17 12:30:31 +0200

    updated the why with buttons

--- resources/views/the_why.blade.php
+++ resources/views/the_why.blade.php
@@ -1,6 +1,47 @@
 @extends ('layout/master')
 
-@include('layout/nav')
+{{-- @include('layout/nav') --}}
+
+<div class="navbar">
+    <div class="nav_bubble">
+        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__dreams">
+            {{-- the <br> why --}}
+            <img class="nav_button_lamp"src="/img/lamp.svg">
+        </a>
+        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+            <h1 class="nav_bubble_text">people's dreams</h1>
+        </a>
+    </div>
+
+    <div class="nav_bubble">
+        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__inspired ">
+            {{-- get <br> inspired --}}
+            <img class="nav_button_lamp"src="/img/lamp.svg">
+        </a>
+        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+            <h1 class="nav_bubble_text">get inspired </h1>
+        </a>
+    </div>
+    <div class="nav_bubble">
+        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__wish">
+            {{-- share <br> your <br> wish --}}
+            <img class="nav_button_lamp"src="/img/lamp.svg">
+        </a>
+        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+            <h1 class="nav_bubble_text">share your wish</h1>
+        </a>
+    </div>
+
+    <div class="nav_bubble">
+        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__home">
+            {{-- people's <br> dreams --}}
+            <img class="nav_button_genie"src="/img/genie_button.svg">
+        </a>
+        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+            <h1 class="nav_bubble_text">home</h1>
+        </a>
+    </div>
+</div>
 
 <div class="title">
     <h1 class="generic__title">the why</h1>
