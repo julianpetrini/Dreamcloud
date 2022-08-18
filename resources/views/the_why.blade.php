d04fad11f206350d0509407f08358688132f65f2
julianpetrini <bwmisojos@gmail.com>
2022-08-18 11:01:10 +0200

    dreams design mobile finished*

--- resources/views/the_why.blade.php
+++ resources/views/the_why.blade.php
@@ -4,40 +4,40 @@
 
 <div class="navbar">
     <div class="nav_bubble">
-        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__dreams">
+        <a href="dreams" class="bubble__link nav_bubble__dreams">
             {{-- the <br> why --}}
             <img class="nav_button_lamp"src="/img/lamp.svg">
         </a>
-        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+        <a href="dreams"  class="bubble__text">
             <h1 class="nav_bubble_text">people's dreams</h1>
         </a>
     </div>
 
     <div class="nav_bubble">
-        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__inspired ">
+        <a href="inspired" class="bubble__link nav_bubble__inspired ">
             {{-- get <br> inspired --}}
             <img class="nav_button_lamp"src="/img/lamp.svg">
         </a>
-        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+        <a href="inspired" class="bubble__text">
             <h1 class="nav_bubble_text">get inspired </h1>
         </a>
     </div>
     <div class="nav_bubble">
-        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__wish">
+        <a href="share" class="bubble__link nav_bubble__wish">
             {{-- share <br> your <br> wish --}}
             <img class="nav_button_lamp"src="/img/lamp.svg">
         </a>
-        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+        <a href="share" class="bubble__text">
             <h1 class="nav_bubble_text">share your wish</h1>
         </a>
     </div>
 
     <div class="nav_bubble">
-        <a href="https://www.google.com/" target="_blank" class="bubble__link nav_bubble__home">
+        <a href="/" class="bubble__link nav_bubble__home">
             {{-- people's <br> dreams --}}
             <img class="nav_button_genie"src="/img/genie_button.svg">
         </a>
-        <a href="https://www.google.com/" target="_blank" class="bubble__text">
+        <a href="/" class="bubble__text">
             <h1 class="nav_bubble_text">home</h1>
         </a>
     </div>
