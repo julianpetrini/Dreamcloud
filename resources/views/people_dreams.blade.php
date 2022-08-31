b1975ab00585b7935c3bf9c759f7cb3b4c8ad874
julianpetrini <bwmisojos@gmail.com>
2022-08-31 12:32:24 +0200

    people_dreams updated

--- resources/views/people_dreams.blade.php
+++ resources/views/people_dreams.blade.php
@@ -582,8 +582,9 @@
             <a href="/result/{{ $message->id }}" class="no_underline">
                 <div class="dreams__box__{{ $message->wish }}" name="{{ $message->wish }}">
                     <div class="dreams__box__header">
-                        <h3 class="dreams__box__title">{{ $message->username }}</h3>
-                        <h3 class="dreams__box__title">{{ $message->created_at->format('Y-m-d') }}</h3>
+                        {{-- <h3 class="dreams__box__title">{{ $message->username }}</h3>
+                        <h3 class="dreams__box__title">{{ $message->created_at->format('Y-m-d') }}</h3> --}}
+                        {{-- OPCION PARA VER SI SOLO MUESTRO EL SUENO O CON LA INFO TAMBIEN INCLUIDA --}}
                     </div>
                     <p class="dreams__box__content">{{ $message->comment }}</p>
                 </div>
