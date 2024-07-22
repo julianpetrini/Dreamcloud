2bab7442041ad02d11ab02714e955c47e1f70f07
jpet <jpetzurich@gmail.com>
2024-07-22 19:51:22 +0200

    Textos de dreams ajustados

--- database/migrations/2022_08_25_090235_create_blogs_table.php
+++ database/migrations/2022_08_25_090235_create_blogs_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('blogs', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('author');
             $table->string('title');
             $table->text('content');
