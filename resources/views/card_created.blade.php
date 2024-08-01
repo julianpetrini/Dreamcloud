d552fc729a5c9b17fa33f68d811adfe283d1af5c
jpet <jpetzurich@gmail.com>
2024-08-01 14:11:50 +0200

    Imagenes a nivel local y funcionando en ambas card_created y dream_download

--- resources/views/card_created.blade.php
+++ resources/views/card_created.blade.php
@@ -59,19 +59,35 @@
     <script type="text/javascript">
         function getRandomImage() {
             var randomImage = [
-                "https://upload.wikimedia.org/wikipedia/commons/a/aa/Arthur-Rackham-Aladdin-New-lamps-for-old.jpg",
-                "https://www.pookpress.co.uk/wp-content/uploads/2019/03/Aladdin-and-the-Wonderful-Lamp_Edmund-Dulac.png",
-                "https://upload.wikimedia.org/wikipedia/commons/6/66/Alad.jpg",
-                "https://i.pinimg.com/550x/75/50/8c/75508cf75ed680cc02b99cfc8e0d0a9e.jpg",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Arthur-Pyle_The_Enchanter_Merlin.JPG/1200px-Arthur-Pyle_The_Enchanter_Merlin.JPG",
-                "https://i.pinimg.com/originals/31/3f/f1/313ff10516fc577859538858c414e325.jpg",
-                "https://i.pinimg.com/originals/46/d9/de/46d9decb142f90f7885cfd348e49176c.jpg",
-                "https://www.oldbookillustrations.com/wp-content/uploads/2016/12/golden-dream.jpg",
-                "https://media.istockphoto.com/illustrations/joseph-interprets-pharaohs-dreams-steel-engraving-published-1841-illustration-id543808004?k=20&m=543808004&s=612x612&w=0&h=66tZBXzgWDDaFKwbVafEUR2LmrxahVAx_cKaXvw8x6s=",
-                "https://i.pinimg.com/originals/c2/69/59/c26959ae3cb9b68e2dcaff3fc1128ee5.jpg"
+                "card (10).jpg",
+                "card (11).jpg",
+                "card (12).jpg",
+                "card (13).jpg",
+                "card (14).jpg",
+                "card (15).jpg",
+                "card (16).jpg",
+                "card (17).jpg",
+                "card (18).png",
+                "card (19).png",
+                "card (1).jpg",
+                "card (2).jpg",
+                "card (3).jpg",
+                "card (4).jpg",
+                "card (5).jpg",
+                "card (6).jpg",
+                "card (7).jpg",
+                "card (8).jpg",
+                "card (9).jpg"
             ];
+
+            
             var number = Math.floor(Math.random() * randomImage.length);
-            document.getElementById("card__image").innerHTML = '<img class="card__box__img__responsive" src="' + randomImage[number] + '" onload="imageLoaded()" />';
+            var imgElement = document.createElement('img');
+
+            imgElement.src = "/img/Cards/" + randomImage[number];
+            imgElement.className = 'card__box__img__responsive';
+            imgElement.onload = imageLoaded;
+            document.getElementById("card__image").appendChild(imgElement);
         }
 
         function imageLoaded() {
@@ -94,16 +110,14 @@
             }).then(function(canvas) {
                 var imgData = canvas.toDataURL("image/png");
 
+                // Crear enlace para descargar la imagen
                 var link = document.createElement('a');
                 link.href = imgData;
-                link.download = 'card_image.png';
+                link.download = 'dreamcloud_dream.png';
                 link.click();
 
-                document.getElementById("soon").innerHTML = `
-                    <button onclick="shareOnSocial('whatsapp', '${imgData}')">Share on WhatsApp</button>
-                    <button onclick="shareOnSocial('facebook', '${imgData}')">Share on Facebook</button>
-                    <button onclick="shareOnSocial('instagram', '${imgData}')">Share on Instagram</button>
-                `;
+                // Actualizar la interfaz para compartir
+               
             }).catch(function(error) {
                 console.error('Error capturing screenshot:', error);
             }).finally(function() {
@@ -111,21 +125,11 @@
             });
         }
 
-        function shareOnSocial(platform, imgData) {
-            var url;
-            switch (platform) {
-                case 'whatsapp':
-                    url = `https://api.whatsapp.com/send?text=${encodeURIComponent('Check out this image: ')} ${imgData}`;
-                    break;
-                case 'facebook':
-                    url = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(imgData)}`;
-                    break;
-                case 'instagram':
-                    alert("Instagram sharing is not supported directly via web. Please download the image and share manually.");
-                    return;
-            }
-            window.open(url, '_blank');
-        }
+        
     </script>
-@include('layout/footer')
+
+    <div class="soon__text" id="soon"></div>
+    <div id="output" class="photo_output"></div>
+
+    @include('layout/footer')
 </body>
--- resources/views/dream_download.blade.php
+++ resources/views/dream_download.blade.php
@@ -61,20 +61,32 @@
     <script type="text/javascript">
         function getRandomImage() {
             var randomImage = [
-                "https://upload.wikimedia.org/wikipedia/commons/a/aa/Arthur-Rackham-Aladdin-New-lamps-for-old.jpg",
-                "https://www.pookpress.co.uk/wp-content/uploads/2019/03/Aladdin-and-the-Wonderful-Lamp_Edmund-Dulac.png",
-                "https://upload.wikimedia.org/wikipedia/commons/6/66/Alad.jpg",
-                "https://i.pinimg.com/550x/75/50/8c/75508cf75ed680cc02b99cfc8e0d0a9e.jpg",
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Arthur-Pyle_The_Enchanter_Merlin.JPG/1200px-Arthur-Pyle_The_Enchanter_Merlin.JPG",
-                "https://i.pinimg.com/originals/31/3f/f1/313ff10516fc577859538858c414e325.jpg",
-                "https://i.pinimg.com/originals/46/d9/de/46d9decb142f90f7885cfd348e49176c.jpg",
-                "https://www.oldbookillustrations.com/wp-content/uploads/2016/12/golden-dream.jpg",
-                "https://media.istockphoto.com/illustrations/joseph-interprets-pharaohs-dreams-steel-engraving-published-1841-illustration-id543808004?k=20&m=543808004&s=612x612&w=0&h=66tZBXzgWDDaFKwbVafEUR2LmrxahVAx_cKaXvw8x6s=",
-                "https://i.pinimg.com/originals/c2/69/59/c26959ae3cb9b68e2dcaff3fc1128ee5.jpg"
+                "card (10).jpg",
+                "card (11).jpg",
+                "card (12).jpg",
+                "card (13).jpg",
+                "card (14).jpg",
+                "card (15).jpg",
+                "card (16).jpg",
+                "card (17).jpg",
+                "card (18).png",
+                "card (19).png",
+                "card (1).jpg",
+                "card (2).jpg",
+                "card (3).jpg",
+                "card (4).jpg",
+                "card (5).jpg",
+                "card (6).jpg",
+                "card (7).jpg",
+                "card (8).jpg",
+                "card (9).jpg"
             ];
+
+            
             var number = Math.floor(Math.random() * randomImage.length);
             var imgElement = document.createElement('img');
-            imgElement.src = randomImage[number];
+
+            imgElement.src = "/img/Cards/" + randomImage[number];
             imgElement.className = 'card__box__img__responsive';
             imgElement.onload = imageLoaded;
             document.getElementById("card__image").appendChild(imgElement);
@@ -103,7 +115,7 @@
                 // Crear enlace para descargar la imagen
                 var link = document.createElement('a');
                 link.href = imgData;
-                link.download = 'card_image.png';
+                link.download = 'dreamcloud_dream.png';
                 link.click();
 
                 // Actualizar la interfaz para compartir
